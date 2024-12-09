00184a775a46a31af5563b8a98e212387305bd62
apb9717 <123990526+apb9717@users.noreply.github.com>
2024-12-08 21:30:40 -0500

    Update main.py to allow for frontend origin in CORS

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Replace with the actual origin if different
+    allow_origins=["http://localhost:3000", "https://wings-frontend-server-498263212500.us-central1.run.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
